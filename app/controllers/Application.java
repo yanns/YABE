5eab25db8371acb4cf3f8be4741cd126ba14562a
Yann Simon <simon@adesso.de>
2011-08-15 10:13:30 +0200

    user can comment a post
    Add a simple form so that an user can comment a post.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Required;
 import play.mvc.*;
 
 import java.util.*;
@@ -27,5 +28,16 @@ public class Application extends Controller {
         Post post = Post.findById(id);
         render(post);
     }
-
+    
+    public static void postComment(Long postId, 
+    		                       @Required(message="Author is required") String author, 
+    		                       @Required(message="A message is required") String content) {
+        Post post = Post.findById(postId);
+        if (validation.hasErrors()) {
+            render("Application/show.html", post);
+        }
+        post.addComment(author, content);
+        flash.success("Thanks for posting %s", author);
+        show(postId);
+    }
 }
