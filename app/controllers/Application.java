7607ae3b8c67afe4d9f3cfb8dd102b646a9832c1
Yann Simon <simon@adesso.de>
2011-08-15 10:06:37 +0200

    detail page for a post
    With the display tag, add a detail page for a post.
    Ex: http://localhost:9000/posts/1
    
    Add a pagination to link previous / next post(s).

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -22,5 +22,10 @@ public class Application extends Controller {
         ).from(1).fetch(10);
         render(frontPost, olderPosts);
     }
+    
+    public static void show(Long id) {
+        Post post = Post.findById(id);
+        render(post);
+    }
 
 }
