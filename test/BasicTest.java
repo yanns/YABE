f64471549e17e8cdb0950730b8802db15788e781
Yann Simon <simon@adesso.de>
2011-08-15 09:31:31 +0200

    Test: create, retrieve and connect user
    Run the application with the id 'test'.
    The tests are shown under: http://localhost:9000/@tests
    
    The tests can be integrated into jenkins (https://wiki.jenkins-ci.org/display/JENKINS/play-plugin)

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -6,8 +6,27 @@ import models.*;
 public class BasicTest extends UnitTest {
 
     @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
+    public void createAndRetrieveUser() {
+        // Create a new user and save it
+        new User("bob@gmail.com", "secret", "Bob").save();
+        
+        // Retrieve the user with e-mail address bob@gmail.com
+        User bob = User.find("byEmail", "bob@gmail.com").first();
+        
+        // Test 
+        assertNotNull(bob);
+        assertEquals("Bob", bob.fullname);
+    }
+    
+    @Test
+    public void tryConnectAsUser() {
+        // Create a new user and save it
+        new User("bob@gmail.com", "secret", "Bob").save();
+        
+        // Test 
+        assertNotNull(User.connect("bob@gmail.com", "secret"));
+        assertNull(User.connect("bob@gmail.com", "badpassword"));
+        assertNull(User.connect("tom@gmail.com", "secret"));
     }
 
 }
