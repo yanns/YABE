f100ffe35ead76044a3747d84100cc7b1510f85b
Yann Simon <simon@adesso.de>
2011-08-15 09:40:33 +0200

    add models post and comment
    BasicTest is extended to test the persistence of Comment and Post.
    BasicTest deletes all data in the database before running a test, so that
    each test has a clean database.
    
    Run http://localhost:9000/@tests?select=BasicTest.class without stopping the server
    (all new classes / changed classes were auto-reloaded)

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -4,6 +4,11 @@ import play.test.*;
 import models.*;
 
 public class BasicTest extends UnitTest {
+	
+    @Before
+    public void setup() {
+        Fixtures.deleteDatabase();
+    }
 
     @Test
     public void createAndRetrieveUser() {
@@ -27,6 +32,64 @@ public class BasicTest extends UnitTest {
         assertNotNull(User.connect("bob@gmail.com", "secret"));
         assertNull(User.connect("bob@gmail.com", "badpassword"));
         assertNull(User.connect("tom@gmail.com", "secret"));
+    }
+    
+    @Test
+    public void createPost() {
+        // Create a new user and save it
+        User bob = new User("bob@gmail.com", "secret", "Bob").save();
+        
+        // Create a new post
+        new Post(bob, "My first post", "Hello world").save();
+        
+        // Test that the post has been created
+        assertEquals(1, Post.count());
+        
+        // Retrieve all posts created by Bob
+        List<Post> bobPosts = Post.find("byAuthor", bob).fetch();
+        
+        // Tests
+        assertEquals(1, bobPosts.size());
+        Post firstPost = bobPosts.get(0);
+        assertNotNull(firstPost);
+        assertEquals(bob, firstPost.author);
+        assertEquals("My first post", firstPost.title);
+        assertEquals("Hello world", firstPost.content);
+        assertNotNull(firstPost.postedAt);
+    }
+    
+    @Test
+    public void postComments() {
+        // Create a new user and save it
+        User bob = new User("bob@gmail.com", "secret", "Bob").save();
+     
+        // Create a new post
+        Post bobPost = new Post(bob, "My first post", "Hello world").save();
+     
+        // Post a first comment
+        bobPost.addComment("Jeff", "Nice post");
+        bobPost.addComment("Tom", "I knew that !");
+     
+        // Count things
+        assertEquals(1, User.count());
+        assertEquals(1, Post.count());
+        assertEquals(2, Comment.count());
+     
+        // Retrieve Bob's post
+        bobPost = Post.find("byAuthor", bob).first();
+        assertNotNull(bobPost);
+     
+        // Navigate to comments
+        assertEquals(2, bobPost.comments.size());
+        assertEquals("Jeff", bobPost.comments.get(0).author);
+        
+        // Delete the post
+        bobPost.delete();
+        
+        // Check that all comments have been deleted
+        assertEquals(1, User.count());
+        assertEquals(0, Post.count());
+        assertEquals(0, Comment.count());
     }
 
 }
