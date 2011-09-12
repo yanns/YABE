357d38612143abca8c2951fdefcdf109828125a9
Yann Simon <simon@adesso.de>
2011-09-12 06:34:12 +0200

    only admin users can use CRUD controlers

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -8,4 +8,14 @@ public class Security extends Secure.Security {
         return User.connect(username, password) != null;
     }
     
+    /**
+     * authorization
+     */
+    static boolean check(String profile) {
+        if("admin".equals(profile)) {
+            return User.find("byEmail", connected()).<User>first().isAdmin;
+        }
+        return false;
+    }
+    
 }
